1e2cfa0bff8e19b7e67e70c440ed99979c8095b2
PierreMasselot1 <pierre.masselot.d@gmail.com>
2025-05-08 00:13:48 -0400

    basic home page

--- bikecalc/src/routes/+page.svelte
+++ bikecalc/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	const pages = [
+		{ name: 'Tire Pressure Calculator', path: '/tire_pressure' },
+		{ name: 'Lorem', path: '' },
+		{ name: 'Ipsum', path: '' }
+	];
+</script>
+
+<div class="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+	<div class="grid grid-cols-1 gap-4 md:grid-cols-3">
+		{#each pages as page}
+			<a
+				href={page.path}
+				class="rounded-lg bg-slate-600 px-4 py-2 text-center text-white transition hover:bg-slate-800"
+			>
+				{page.name}
+			</a>
+		{/each}
+	</div>
+</div>
