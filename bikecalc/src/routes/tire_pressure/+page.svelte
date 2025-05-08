58343dba459f8f260ef3c06a5953bd22bc3499ac
PierreMasselot1 <pierre.masselot.d@gmail.com>
2025-05-08 00:36:07 -0400

    couple bike selection

---
+++ bikecalc/src/routes/tire_pressure/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	const bike_types = [
+		{ n: 'TT', c: 'bg-blue-500' },
+		{ n: 'Road', c: 'bg-yellow-500' },
+		{ n: 'Gravel', c: 'bg-neutral-500' },
+		{ n: 'MTB', c: 'bg-green-500' }
+	];
+</script>
+
+<div class="flex items-center">
+	{#each bike_types as bike}
+		<button class="{bike.c} m-2 rounded-md px-4 py-2">
+			{bike.n}
+		</button>
+	{/each}
+</div>
